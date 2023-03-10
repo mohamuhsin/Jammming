91be0150e96be5ee68ecc95d0bad019a3c316dd2
mohamuhsin <muhsinohoho@outlook.com>
2023-03-11 00:43:56 +0300

    updated track component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,14 +2,24 @@ import React from "react";
 
 import "./Track.css";
 class Track extends React.Component {
+
+  renderAction() {
+    if(this.props.isRemoval) {
+      return <Button className="Track-action">-</Button>
+    } else {
+      return <Button className="Track-action">+</Button>
+    }
+  }
+
+
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3>coding</h3>
-          <p>coding for life</p>
+          <h3>Track Name</h3>
+          <p>Track artist | Track album</p>
         </div>
-        <button className="Track-action">{renderAction()}</button>
+       {this.renderAction()}
       </div>
     );
   }
