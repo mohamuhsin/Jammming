c8ed5dede905a007c51bfd199d8c66eefa713fb9
mohamuhsin <muhsinohoho@outlook.com>
2023-03-11 00:48:11 +0300

    created constructor method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,18 @@ import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: {
-        name, artist, album, id
-      }
-    }
+      searchResults: [
+        {
+          name: "name1",
+          artist: "artist1",
+          album: "album1",
+          id: 1,
+        },
+      ],
+    };
   }
   render() {
     return (
