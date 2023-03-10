5cf170913565b453116e0f251c96b93eb0ec294f
mohamuhsin <muhsinohoho@outlook.com>
2023-03-10 23:28:23 +0300

    updated App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      <h1>Jammming </h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SeachBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
