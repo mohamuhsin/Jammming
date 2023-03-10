0e371aa5da002cedfc0b732b30002b5e5e297848
mohamuhsin <muhsinohoho@outlook.com>
2023-03-10 23:52:29 +0300

    updated files

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+
 import "./SearchResults.css";
 import TrackList from '../TrackList/TrackList';
 
